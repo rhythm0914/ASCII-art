daba1d243005708e81fca54eea3e0070595939d4
Ryan Joseph Canano <56010462+rhythm0914@users.noreply.github.com>
2024-08-29 19:25:38 -0700

    Add files via upload
    Function Definitions:
    print_ryan():
    print_joseph():
      Defines two variables: blue and reset, which contain ANSI escape codes for text color formatting.
    
    Escape Sequences: The \033[34m sequence sets the text color to blue, while \033[0m resets the color.
     blue = '\033[34m' sets the text color to blue.
     reset = '\033[0m' resets the text formatting to default.
    
    Raw Strings: Utilizes raw string formatting (rf"") to handle special characters and escape sequences seamlessly.
     The rf"" syntax allows for raw string formatting. Raw strings treat backslashes (\) as literal characters, and the f allows for variable interpolation.

---
+++ Ryan.py
@@ -0,0 +1,23 @@
+def print_ryan():
+    blue = '\033[34m'
+    reset = '\033[0m'
+    print(rf"""{blue}
+
+  _ \ \ \  /  \     \ | 
+    /  \  /  _ \   .  | 
+ _|_\   _| _/  _\ _|\_| 
+                        
+{reset}""")
+def print_joseph():
+    blue = '\033[34m'
+    reset = '\033[0m'
+    print(rf"""{blue} 
+
+     |  _ \    __|  __|  _ \ |  | 
+  \  | (   | \__ \  _|   __/ __ | 
+ \__/ \___/  ____/ ___| _|  _| _| 
+                                  
+{reset}""")
+
+print_ryan()
+print_joseph()
